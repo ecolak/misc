70150513ecbb096db6dde0d9b9c3d9cfa37ed318
Emre Colak <colemre@gmail.com>
2013-07-04 00:54:29 -0700

    Cleaning up some the allow-origin filter

--- olasihaber/src/main/java/com/abe/olasihaber/ws/ArticleService.java
+++ olasihaber/src/main/java/com/abe/olasihaber/ws/ArticleService.java
@@ -14,19 +14,16 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import java.util.List;
+
 import com.abe.olasihaber.dao.MockData;
+import com.abe.olasihaber.model.Article;
+import com.abe.olasihaber.model.Arguments;
 import com.abe.olasihaber.model.Vote;
 
 @Path("/articles")
 @Produces(MediaType.APPLICATION_JSON)
 public class ArticleService {
-
-	private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-	private static final String LOCALHOST = "http://localhost:280";
-	private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
-	private static final String ALLOWED_METHODS = "POST, GET, OPTIONS, DELETE, PUT";
-	private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
-	private static final String ALLOWED_HEADERS = "X-Requested-With, Content-Type";
 
 	@Context
 	UriInfo uriInfo;
@@ -35,20 +32,20 @@ public class ArticleService {
 	HttpServletRequest request;
 	
 	@GET
-	public Response getAll() {
-		return makeResponse(MockData.getAllArticles()); 
+	public List<Article> getAll() {
+		return MockData.getAllArticles(); 
 	}
 
 	@GET
 	@Path("{id}")
-	public Response getById(@PathParam("id") long id) {
-		return makeResponse(MockData.getArticleById(id));
+	public Article getById(@PathParam("id") long id) {
+		return MockData.getArticleById(id);
 	}
 	
 	@GET
 	@Path("{id}/arguments")
-	public Response getArguments(@PathParam("id") long id) {
-		return makeResponse(MockData.getArgumentsForArticle(id));
+	public Arguments getArguments(@PathParam("id") long id) {
+		return MockData.getArgumentsForArticle(id);
 	}
 	
 	@POST
@@ -57,13 +54,13 @@ public class ArticleService {
 		System.out.println("ip " + request.getRemoteAddr() + " voting " + vote.getVote_type() + 
 							" article " + vote.getArticle_id());
 		MockData.saveVote(new Vote(vote.getArticle_id(), request.getRemoteAddr(), vote.getVote_type()));
-		return makeResponse(null);
+		return Response.ok().build();
 	}
 	
 	@GET
 	@Path("{id}/vote")
-	public Response getVote(@PathParam("id") long id) {
-		return makeResponse(MockData.getVoteByIpAndArticle(request.getRemoteAddr(), id));
+	public Vote getVote(@PathParam("id") long id) {
+		return MockData.getVoteByIpAndArticle(request.getRemoteAddr(), id);
 	}
 	
 	@PUT
@@ -72,7 +69,7 @@ public class ArticleService {
 		System.out.println("ip " + request.getRemoteAddr() + " voting " + vote.getVote_type() + 
 							" article " + vote.getArticle_id());
 		MockData.saveVote(new Vote(vote.getArticle_id(), request.getRemoteAddr(), vote.getVote_type()));
-		return makeResponse(null);
+		return Response.ok().build();
 	}
 	
 	@DELETE
@@ -81,14 +78,7 @@ public class ArticleService {
 		System.out.println("ip " + request.getRemoteAddr() + " went undecided about article " +
 							" article " + id);
 		MockData.deleteVoteByIpAndArticle(request.getRemoteAddr(), id);
-		return makeResponse(null);
+		return Response.ok().build();
 	}
-	
-	private Response makeResponse(Object entity) {
-		ResponseBuilder rb = entity != null ? Response.ok(entity) : Response.ok();
-		return rb
-				.header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-				.header(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS)
-				.header(ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS).build();
-	}
+
 }
