b6570178367ff6352f6fe5c64dc5398ce5c245d7
Emre Colak <colemre@gmail.com>
2018-08-03 13:54:28 -0700

    Separate acronyms

--- ask-helix/src/main/java/emre/colak/helixinsights/util/AlexaUtils.java
+++ ask-helix/src/main/java/emre/colak/helixinsights/util/AlexaUtils.java
@@ -1,10 +1,18 @@
 package emre.colak.helixinsights.util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class AlexaUtils {
 
   private AlexaUtils() {}
+  
+  private static final Set<String> acronyms = new HashSet<>();
+  static {
+    acronyms.add("DNA");
+    acronyms.add("ACG");
+  }
   
   public static <T> String listToSSML(List<T> list) {
     return listToSSML(list, null);
@@ -19,8 +27,24 @@ public class AlexaUtils {
       sb.append("<p>").append(title).append("</p>");
     }
     for (T t : list) {
-      sb.append("<p>").append(t.toString()).append("</p>");
+      sb.append("<p>").append(separateAcronyms(t.toString())).append("</p>");
     }
     return sb.toString();
+  }
+  
+  private static String separateAcronyms(String s) {
+    for (String acr : acronyms) {
+      if (s.equalsIgnoreCase(acr)) {
+        return s;
+      }
+      if (s.startsWith(acr)) {
+        s = s.replace(acr, acr + " ");
+      } else if (s.endsWith(acr)) {
+        s = s.replace(acr, " " +  acr);
+      } else if (s.contains(acr)) {
+        s = s.replace(acr, " " +  acr + " ");
+      }
+    }
+    return s;
   }
 }
