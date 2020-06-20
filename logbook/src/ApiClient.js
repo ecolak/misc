f54c008460def5faaeec1edc2eb51c89003a7c36
Emre Colak <colemre@gmail.com>
2020-06-19 17:35:13 -0700

    removed unused code

--- logbook/src/ApiClient.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const ApiClient = axios.create({
-    baseURL: 'https://v0t6ppyvmd.execute-api.us-east-1.amazonaws.com/default/api/trip',
-    headers: {'Authorization': 'S3tWy6Qkqj37LGtD', 'X-USER-ID': 'US-1234'}
-});
-
-export default ApiClient;
